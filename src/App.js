82c93e3003d2ced939611280b98ae9cf1b051cd4
PrakashSharma2000 <ps4687675@gmail.com>
2020-11-30 22:59:42 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -3,32 +3,47 @@ import Header from './component/Header'
 import Books from './component/Books'
 import './App.css'
 import axios from 'axios'
+import Pagination from './component/Pagination'
+
+
+
 
 function App() {
-  const data =[
-    1,2,3,4,5,6,7,8,9,0
-  ]
+ 
   const [items,setItems] =useState([])
   const [isLoading,setIsLoading]=useState(true)
+
+//Pagination part
+  const [currentPage, setcurrentPage]=useState(1)
+  const [page]=useState(12)
 
   useEffect( () => {
     const fetchItems =async () =>{
       const result = await axios(`https://covid19-api.org/api/status`)
       console.log(result.data)
       setItems(result.data)
-      setIsLoading(false)
+      setIsLoading(true)
     }
     fetchItems()
   }, [])
 
+  const indexOfLastPage = currentPage * page;
+  const indexofFirstPage = indexOfLastPage - page;
+  const currentPost = items.slice(indexofFirstPage ,indexOfLastPage);
+
+ 
+  const paginate = pageNumber => setcurrentPage(pageNumber);
 
   return (
     <div className="App">
       <Header/>
-      <Books isloading={isLoading} items={items} data={data} key={items.id}/>
+      <Books isloading={isLoading} items={currentPost} />
+      <Pagination postsPerPage={page} totalpage ={items.length} paginate={paginate}/>
       
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/component/Pagination.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Pagination = ({postsPerPage , totalpage ,paginate}) => {
+
+    const pageNumber = [];
+
+    for(let i=1;i<=Math.ceil(totalpage / postsPerPage); i++){
+        pageNumber.push(i);
+    }
+    return (
+        <div>
+            <nav>
+                <ul>{pageNumber.map(number =>(
+                    <li className="pagUL"><a href="!#" onClick={() => paginate(number)}>{number}</a></li>
+                ))}
+                
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default Pagination
